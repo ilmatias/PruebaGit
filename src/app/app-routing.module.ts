6bd012c058e3c69e4916eb2a4aa7810cfdc23451
ilmatias <matiasplus113@gmail.com>
2023-08-30 00:52:16 -0400

    Creacion Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'form',
+    loadChildren: () => import('./form/form.module').then( m => m.FormPageModule)
+  },
 ];
 
 @NgModule({
